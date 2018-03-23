4a84a045da93e71c6fb073586a9be7519a21f4f5
HiQiQi428 <2725115515@qq.com>
2018-03-23 15:09:56 +0800

    upload

--- Site/dynamic/member.js
+++ Site/dynamic/member.js
@@ -7,20 +7,9 @@ let express=require('express'),
     urlencodedParser=bodyParser.urlencoded({extended:false});
 
 let app=express();
-app.get('/getMembers',(req,rep)=>{
-  if(req.allowed){
-    dbAccess.execute('select memid,pertag,account,headimg from Member',(err,ret)=>{
-      if(err){
-        rep.end(JSON.stringify({statuscode:201,description:util.inspect(err)}));
-        console.log(err);
-      }
-      else rep.end(JSON.stringify({statuscode:200,data:ret}));
-    });
-  }
-});
 app.get('/getOldmsg',(req,rep)=>{
   if(req.allowed){
-    dbAccess.execute('select source,target,message,timestamp from Oldmsg where source='+req.session.memid+' or target='+mmid,(err,ret)=>{
+    dbAccess.execute('select source,target,message,timestamp from Oldmsg where source='+req.session.memid+' or target='+req.session.memid,(err,ret)=>{
       if(err){
         rep.end(JSON.stringify({statuscode:201,description:util.inspect(err)}));
         console.log(err);
@@ -31,7 +20,7 @@ app.get('/getOldmsg',(req,rep)=>{
 });
 app.get('/getNewmsg',(req,rep)=>{
   if(req.allowed){
-    dbAccess.execute('select source,target,message,timestamp from Newmsg where source='+req.session.memid+' or target='+mmid,(err,ret)=>{
+    dbAccess.execute('select source,target,message,timestamp from Newmsg where source='+req.session.memid+' or target='+req.session.memid,(err,ret)=>{
       if(err){
         rep.end(JSON.stringify({statuscode:202,description:util.inspect(err)}));
         console.log(err);
@@ -42,45 +31,70 @@ app.get('/getNewmsg',(req,rep)=>{
 });
 
 let context=[];
+const equalString=(a,b)=>{
+  a=a.replace(/ /g,'+');
+  if(a.length==b.length){
+    for(let i=0,limit=a.length;i<limit;i++) if(a[i]!=b[i]) return false;
+    return true;
+  }
+  return false;
+}
 app.chat=(ws,req)=>{
-  ws.on('message',(msg)=>{
-    if(msg.startsWith('[:MA:]')){// regis
-      let memid=msg.replace('[:MA:]','');
-      // broadcast online
-      for(let key in context){
-        if(context[key]){
-          ws.send('[:MA:]'+key+'[:MA:]1');
-          context[key].send('[:MA:]'+memid+'[:MA:]1');
+  // type src tar cookie status data
+  let client;
+  ws.on('message',(raw)=>{
+    let pack=JSON.parse(raw);
+    if(equalString(pack.cookie,req.cookies['MACookie'])){
+      if(pack.type=='heart'){
+        client=pack.src;
+        for(let key in context){
+          if(context[key]){
+            ws.send(JSON.stringify({type:'updMember',tar:key,data:1}));
+            context[key].send(JSON.stringify({type:'updMember',tar:pack.src,data:1}));
+          }
         }
+        context[pack.src]=ws;
       }
-      // add member
-      context[memid]=ws;
-      ws.send('[:MA:]'+memid+'[:MA:]1');
-    }
-    else{// conversion
-      let [source,target,message]=msg.split('[:MA:]');// missing cookie
-      if(source==req.session.memid){
-        if(context[target]){
-          context[target].send(msg);
-          // save msg into oldmsg
-          console.log('save msg into oldmsg:'+msg);
+      else if(pack.type=='sendMsg'&&pack.src&&pack.tar){
+        if(context[pack.tar]){
+          context[pack.tar].send(raw);
+          dbAccess.execute(`insert into Oldmsg(source,target,message,timestamp) values(
+            `+pack.src+`,
+            `+pack.tar+`,
+            "`+pack.data+`",
+            "`+sillyDate.format(new Date(),'YYYY-MM-DD-hh-mm-ss')+`"
+          )`,(err,rows)=>{
+            if(err) console.log(err);
+            else console.log('old message from src: '+pack.src);
+          });
         }
         else{
-          // save msg into newmsg
-          console.log('save msg into newmsg:'+msg);
+          dbAccess.execute(`insert into Newmsg(source,target,message,timestamp) values(
+            `+pack.src+`,
+            `+pack.tar+`,
+            "`+pack.data+`",
+            "`+sillyDate.format(new Date(),'YYYY-MM-DD-hh-mm-ss')+`"
+          )`,(err,rows)=>{
+            if(err) console.log(err);
+            else console.log('new message from src: '+pack.src);
+          });
         }
+      }
+      else if(pack.type='reqMember'){
+        dbAccess.execute('select memid,pertag,account,headimg from Member',(err,rows)=>{
+          if(err) console.log(err);
+          else ws.send(JSON.stringify({type:'reqMember',data:rows}));
+        });
       }
     }
   });
-  ws.on('close',()=>{// unregis
-    for(let key in context){
-      if(context[key]==ws){
-        context[key]=undefined;
-        // broadcast offline
-        for(let akey in context){
-          if(akey!=key) context[akey].send('[:MA:]'+key+'[:MA:]0');
-        }
-        break;
+  ws.on('close',()=>{
+    if(context[client]){
+      context[client]=undefined;
+      // broadcast offline
+      for(let key in context){
+        if(context[key])
+          context[key].send(JSON.stringify({type:'updMember',tar:client,data:0}));
       }
     }
   });
--- Site/static/js/home/Member.js
+++ Site/static/js/home/Member.js
@@ -1,24 +1,67 @@
 
 let chatbox=$('#Members .ChatBox .content');
 let ws=new WebSocket('ws://'+window.location.host+'/chat');
+ws.onopen=()=>{
+  setTimeout(()=>ws.send(JSON.stringify({type:'heart',src:window.localStorage.memid,cookie:$.cookie('MACookie')})),500);
+  ws.send(JSON.stringify({type:'reqMember',cookie:$.cookie('MACookie')}));
+}
 ws.onmessage=(evt)=>{
-  if(evt.data.startsWith('[:MA:]')){
-    // set offline or online
-    let [target,state]=evt.data.replace('[:MA:]','').split('[:MA:]');
+  let pack=JSON.parse(evt.data);
+  if(pack.type=='updMember'){
     $('#Members .MemberList .item').each((i,elem)=>{
-      if($(elem).attr('memid')==target){
+      if($(elem).attr('memid')==pack.tar){
         let kl=$(elem)[0].classList;
-        if(state&&kl.contains('offline')) kl.remove('offline');
-        if(!state&&!kl.contains('offline')) kl.add('offline');
+        if(pack.data&&kl.contains('offline')) kl.remove('offline');
+        if(!pack.data&&!kl.contains('offline')) kl.add('offline');
       }
     });
   }
-  else chatbox.append(`<div class='item'>
-    <img class='headimg' src='img/defaultHeadimg.png'>
-    <div class='message'><span>`+evt.data.split('[:MA:]')[2]+`</span></div>
-  </div>`);
+  else if(pack.type=='reqMember'){
+    let html='';
+    for(let item of pack.data){
+      if(item.memid!=window.localStorage.memid){
+        let headimg='img/defaultHeadimg.png';
+        if(item.headimg) headimg=item.headimg;
+        html+=`<div class='item offline' memid=`+item.memid+`>
+          <img class='headimg' src='`+headimg+`'>
+          <a class='account'>`+item.account+`</a>
+        </div>`;
+      }
+    }
+    $('#Members .MemberList').html(html);
+    // memberItemActionBind
+    $('#Members .MemberList .item').each((i,elem)=>{
+      let mask;
+      $(elem).bind({
+        mouseenter:()=>{
+          if(mask) mask.remove();
+          mask=$(`<a style='
+          display:block;
+          position:absolute;
+          height:30px;width:100%;
+          left:-100%;top:0px;
+          background:rgba(100,80,200,0.6);
+          '></a>`);
+          mask.appendTo(elem);
+          mask.animate({left:0},300);
+        },
+        mouseleave:()=>mask.animate({left:'100%'},300),
+        mousedown:()=>mask.css('background-color','rgba(100,80,160,1)'),
+        mouseup:()=>mask.css('background-color','rgba(100,80,200,0.6)'),
+        click:()=>{
+          chatTarget=$(elem).attr('memid');
+          inputfield.attr('placeholder','to '+$(elem).find('.account').html()+'...');
+        }
+      });
+    });
+  }
+  else if(pack.type=='sendMsg'){
+    chatbox.append(`<div class='item'>
+      <img class='headimg' src='img/defaultHeadimg.png'>
+      <div class='message'><span>`+pack.data+`</span></div>
+    </div>`);
+  }
 }
-ws.onopen=()=>ws.send('[:MA:]'+window.localStorage.memid);
 // send
 let inputfield=$('#Members .ChatBox .inputfield'),chatTarget;
 inputfield.keypress((e)=>{
@@ -27,7 +70,9 @@ inputfield.keypress((e)=>{
       <img class='headimg' src='img/defaultHeadimg.png'>
       <div class='message'><span>`+inputfield.val()+`</span></div>
     </div>`);
-    ws.send(window.localStorage.memid+'[:MA:]00000000002[:MA:]'+inputfield.val());
+    ws.send(JSON.stringify({
+      type:'sendMsg',status:'',src:window.localStorage.memid,tar:chatTarget,cookie:$.cookie('MACookie'),data:inputfield.val()
+    }))
     inputfield.val('');
     e.stopPropagation();
     return false;
@@ -38,56 +83,7 @@ inputfield.bind('input propertychange','textarea',()=>{
   inputfield.css('height',inputfield[0].scrollHeight);
 });
 
-$(window).on('beforeunload',()=>{
-  ws.close();
-});
 
-const getMemberList=()=>{
-  $.get({
-    url:global_host+'member/getMembers',
-    dataType:'JSON',
-    success:(rep)=>{
-      let container=$('#Members .MemberList');
-      if(rep.statuscode==200&&rep.data.length>0){
-        let html='';
-        for(let item of rep.data){
-          let headimg='img/defaultHeadimg.png';
-          if(item.headimg) headimg=item.headimg;
-          html+=`<div class='item offline' memid=`+item.memid+`>
-            <img class='headimg' src='`+headimg+`'>
-            <a class='account'>`+item.account+`</a>
-          </div>`
-        }
-        container.html(html);
-        // memberItemActionBind
-        $('#Members .MemberList .item').each((i,elem)=>{
-          let mask;
-          $(elem).bind({
-            mouseenter:()=>{
-              if(mask) mask.remove();
-              mask=$(`<a style='
-              display:block;
-              position:absolute;
-              height:30px;width:100%;
-              left:-100%;top:0px;
-              background:rgba(100,80,200,0.6);
-              '></a>`);
-              mask.appendTo(elem);
-              mask.animate({left:0},300);
-            },
-            mouseleave:()=>mask.animate({left:'100%'},300),
-            mousedown:()=>mask.css('background-color','rgba(100,80,160,1)'),
-            mouseup:()=>mask.css('background-color','rgba(100,80,200,0.6)'),
-            click:()=>chatTarget=$(elem).attr('memid')
-          });
-        });
-      }
-      else{
-        console.log('request for member list failed');
-      }
-    }
-  })
-};
 const chatboxAddItems=(msgs)=>{
   for(let item of msgs){
     let pos='';
@@ -99,19 +95,20 @@ const chatboxAddItems=(msgs)=>{
   }
 };
 const getOldmsg=()=>$.get({
-  url:global_host+'member/getOldmsg?user='+window.localStorage.memid,
+  url:global_host+'member/getOldmsg?',
   dataType:'JSON',
   success:(rep)=>{
-    if(rep.statuscode==200) chatboxAddItem(rep.data);
+    console.log(rep)
+    if(rep.statuscode==200) chatboxAddItems(rep.data);
     else if(rep.statuscode==201) console.log('no old msg');// no old msg
     else notify(rep.description);// error
   }
 });
 const getNewmsg=()=>$.get({
-  url:global_host+'member/getNewmsg?user='+window.localStorage.memid,
+  url:global_host+'member/getNewmsg?',
   dataType:'JSON',
   success:(rep)=>{
-    if(rep.statuscode==200) chatboxAddItem(rep.data);
+    if(rep.statuscode==200) chatboxAddItems(rep.data);
     else if(rep.statuscode==201) console.log('no new msg');// no old msg
     else notify(rep.description);// error
   },
@@ -123,9 +120,11 @@ const getTargetProfile=(target)=>{
 
 };
 
-getMemberList();
-// getOldmsg();
-// getNewmsg();
+getOldmsg();
+getNewmsg();
 
-// 1.chat,redius
-// 2.get detail
+$(window).on('beforeunload',()=>{
+  ws.close();
+  getOldmsg();
+  getNewmsg();
+});
